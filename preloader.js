fc9d9833b2665eba24eab21b9f6464fbb4e70dee
Yazid85 <81072709+Yazid85@users.noreply.github.com>
2022-08-22 20:03:16 +0700

    Create preloader.js

---
+++ preloader.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var preloader = document.querySelector('.preloader');
+    setTimeout(function() {
+        preloader.classList.add('hidden')
+    }, 7000)
+});
